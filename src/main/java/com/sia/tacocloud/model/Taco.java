66816c4b101b055d1fa0cf717171671ebc7dae3a
Aleksandr Smirnov <skremeone1@gmail.com>
2024-06-09 21:13:43 +0300

    models added

---
+++ src/main/java/com/sia/tacocloud/model/Taco.java
@@ -0,0 +1,12 @@
+package com.sia.tacocloud.model;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class Taco {
+
+    private String name;
+    private List<Ingredient> ingredients;
+}
