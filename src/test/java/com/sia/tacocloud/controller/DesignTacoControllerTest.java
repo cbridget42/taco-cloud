c813ebba1f98dcd6516e33656ed189c19a9da9bb
Aleksandr Smirnov <skremeone1@gmail.com>
2024-06-13 17:25:02 +0300

    Order logic

---
+++ src/test/java/com/sia/tacocloud/controller/DesignTacoControllerTest.java
@@ -0,0 +1,29 @@
+package com.sia.tacocloud.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@WebMvcTest(DesignTacoController.class)
+public class DesignTacoControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testShowDesignForm() throws Exception {
+        mockMvc.perform(get("/design"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("design"))
+                .andExpect(content().string(containsString("Design your taco!")))
+                .andExpect(content().encoding("UTF-8"))
+                .andExpect(content().contentType("text/html;charset=UTF-8"));
+    }
+}
