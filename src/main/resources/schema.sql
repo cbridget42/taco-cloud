837230a1a4b05185477962a17ef7c0f61aee4a2e
Aleksandr Smirnov <skremeone1@gmail.com>
2024-08-13 18:35:09 +0300

    switch to external database

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,5 +1,7 @@
-CREATE TABLE IF NOT EXISTS taco_order (
-    id identity,
+CREATE SCHEMA IF NOT EXISTS taco_cloud;
+
+CREATE TABLE IF NOT EXISTS taco_cloud.taco_order (
+    id serial,
     delivery_name varchar(50) not null,
     delivery_street varchar(50) not null,
     delivery_city varchar(50) not null,
@@ -11,25 +13,25 @@ CREATE TABLE IF NOT EXISTS taco_order (
     placed_at timestamp not null
 );
 
-CREATE TABLE IF NOT EXISTS taco (
-    id identity,
+CREATE TABLE IF NOT EXISTS taco_cloud.taco (
+    id serial,
     name varchar(50) not null,
     taco_order bigint not null,
     taco_order_key bigint not null,
     created_at timestamp not null
 );
 
-CREATE TABLE IF NOT EXISTS ingredient_ref (
+CREATE TABLE IF NOT EXISTS taco_cloud.ingredient_ref (
     ingredient varchar(4) not null,
     taco bigint not null,
     taco_key bigint not null
 );
 
-CREATE TABLE IF NOT EXISTS ingredient (
+CREATE TABLE IF NOT EXISTS taco_cloud.ingredient (
     id varchar(4) PRIMARY KEY NOT NULL,
     name varchar(25) not null,
     type varchar(10) not null
 );
 
-ALTER TABLE taco ADD FOREIGN KEY (taco_order) REFERENCES taco_order(id);
-ALTER TABLE ingredient_ref ADD FOREIGN KEY (ingredient) references ingredient(id);
+ALTER TABLE taco_cloud.taco ADD FOREIGN KEY (taco_order) REFERENCES taco_cloud.taco_order(id);
+ALTER TABLE taco_cloud.ingredient_ref ADD FOREIGN KEY (ingredient) REFERENCES taco_cloud.ingredient(id);
